7e061098c768d8c750becfbcec9dc8c302232a72
sohye-pk <sohye.pk@gmail.com>
2026-08-12 18:22:29 +0900

    feat: Quiz 클래스 추가

---
+++ quiz.py
@@ -0,0 +1,22 @@
+class Quiz:
+    def __init__(self, question, choices, answer, hint=None):
+        self.question = question
+        self.choices = choices
+        self.answer = answer
+        self.hint = hint
+
+    def show_question(self):
+        print()
+        print(self.question)
+
+        for index, choice in enumerate(self.choices, start=1):
+            print(f"{index}. {choice}")
+
+    def check_answer(self, user_answer):
+        return user_answer == self.answer
+
+    def show_hint(self):
+        if self.hint:
+            print(f"힌트: {self.hint}")
+        else:
+            print("힌트가 없습니다.")
